6bbe8709177424f34df15f23a7705bd6b862230c
m3hdi-i <m3hdi.i4@gmail.com>
2023-06-17 01:31:04 +0330

    Design AdFragment

--- app/src/main/java/ir/m3hdi/agahinet/ui/fragment/AdFragment.kt
+++ app/src/main/java/ir/m3hdi/agahinet/ui/fragment/AdFragment.kt
@@ -1,14 +1,18 @@
 package ir.m3hdi.agahinet.ui.fragment
 
+import android.R
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import ir.m3hdi.agahinet.databinding.FragmentAdBinding
 import ir.m3hdi.agahinet.domain.model.Ad
+
 
 class AdFragment : Fragment() {
 
@@ -16,6 +20,7 @@ class AdFragment : Fragment() {
     private val binding get() = _binding!!
 
     private val args: AdFragmentArgs by navArgs()
+    private lateinit var ad:Ad
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         _binding = FragmentAdBinding.inflate(inflater, container, false)
@@ -24,8 +29,18 @@ class AdFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        ad=args.ad
 
-        Toast.makeText(requireContext(),args.ad.title,Toast.LENGTH_LONG).show()
+
+        binding.buttonBack.setOnClickListener {
+            findNavController().popBackStack()
+        }
+        binding.checkboxBookmark.setOnCheckedChangeListener { checkBox, isChecked ->
+            // Respond to icon toggle
+        }
+        binding.buttonShare.setOnClickListener {
+
+        }
     }
 
 
